e60158c7e89c88ee6f186acaffd4eb73fae6c3e8
maginfnews <maicon@magpass.com.br>
2025-11-12 17:04:05 -0300

    Configure Google Analytics with real tracking ID G-2B94NM51F9

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,15 +1,12 @@
 import Script from 'next/script';
 
 export default function GoogleAnalytics() {
-  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-
-  if (!GA_MEASUREMENT_ID) {
-    return null;
-  }
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-2B94NM51F9';
 
   return (
     <>
       <Script
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
