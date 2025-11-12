4f2402700e294b4e40557a03ad8c850c9e56b83a
maginfnews <maicon@magpass.com.br>
2025-11-12 15:42:28 -0300

    Configure for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    domains: ['maginfnews.github.io'],
+    unoptimized: false,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
